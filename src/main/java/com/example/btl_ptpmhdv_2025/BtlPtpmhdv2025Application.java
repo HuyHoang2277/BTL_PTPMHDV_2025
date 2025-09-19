6cbb4992810bf0dd7350f37f2291db984ce5f1b8
Nguyễn Huy Hoàng <huyhoangtkcn2277@gmail.com>
2025-09-19 15:37:57 +0700

    Base
    add khung cơ bản

---
+++ src/main/java/com/example/btl_ptpmhdv_2025/BtlPtpmhdv2025Application.java
@@ -0,0 +1,13 @@
+package com.example.btl_ptpmhdv_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlPtpmhdv2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtlPtpmhdv2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/btl_ptpmhdv_2025/BtlPtpmhdv2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btl_ptpmhdv_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlPtpmhdv2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
